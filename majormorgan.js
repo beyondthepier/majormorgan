2a8b288a035bcb9fea40fab956929c71070c6934
Josie <josie@beyondthepier.co.uk>
2020-02-09 17:10:55 +0000

    Add files via upload

---
+++ majormorgan.js
@@ -0,0 +1,114 @@
+  //majormorgan JS
+
+  const $lowa = document.getElementById('lowA')
+  const $lowb = document.getElementById('lowB')
+  const $lowc = document.getElementById('lowC')
+  const $lowd = document.getElementById('lowD')
+  const $lowd2 = document.getElementById('lowD2')
+  const $lowe = document.getElementById('lowE')
+  const $lowf = document.getElementById('lowF')
+  const $lowg = document.getElementById('lowG')
+  const $lowg2 = document.getElementById('lowG2')
+  const $higha = document.getElementById('highA')
+  const $highb = document.getElementById('highB')
+  const $highc = document.getElementById('highC')
+  const $highc2 = document.getElementById('highC2')
+  const $highd = document.getElementById('highD')
+  const $highe = document.getElementById('highE')
+  const $highf = document.getElementById('highF')
+
+$lowa.addEventListener('click',function(){
+        const sound = 
+          new Audio("soundsmm/lowA.mp3");
+      sound.play();
+  },false);
+
+$lowb.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/lowB.mp3");
+    sound.play();
+},false);
+  
+$lowc.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/lowC.mp3");
+    sound.play();
+},false);
+
+$lowd.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/lowD.mp3");
+    sound.play();
+},false);
+
+$lowd2.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/lowD.mp3");
+    sound.play();
+},false);
+
+$lowe.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/lowE.mp3");
+    sound.play();
+},false);
+
+$lowf.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/lowF.mp3");
+    sound.play();
+},false);
+
+$lowg.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/lowG.mp3");
+    sound.play();
+},false);
+
+$lowg2.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/lowG.mp3");
+    sound.play();
+},false);
+
+$higha.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/highA.mp3");
+    sound.play();
+},false);
+
+$highb.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/highB.mp3");
+    sound.play();
+},false);
+
+$highc.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/highC.mp3");
+    sound.play();
+},false);
+
+$highc2.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/highC.mp3");
+    sound.play();
+},false);
+
+$highd.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/highD.mp3");
+    sound.play();
+},false);
+
+$highe.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/highE.mp3");
+    sound.play();
+},false);
+
+$highf.addEventListener('click',function(){
+    const sound =
+        new Audio("soundsmm/highF.mp3");
+    sound.play();
+},false);
